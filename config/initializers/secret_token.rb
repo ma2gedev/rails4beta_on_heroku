5057d44bdc6109030c862810996460d334e7f7fd
ma2ge <takayuki.1229@gmail.com>
2012-07-04 09:02:56 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+Edge::Application.config.secret_token = '69c8a5553fcd84a446d58d664a69ab9dd0ccdffc95443c3528a99387d0ac9c93e24fa003ba3ec61190b4d66e3187badaba30789403d0b661fffe3b5d2c1f5091'
